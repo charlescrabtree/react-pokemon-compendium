b8b550b43e8266a6c8fcda6c762cb583d6481971
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-21 15:59:32 -0700

    added type dropdown

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -1,15 +1,18 @@
 import { useState, useEffect } from 'react';
-import { fetchPokemon } from '../services/pokemon';
+import { fetchPokemon, fetchTypes } from '../services/pokemon';
 
 export function usePokemon() {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedType, setSelectedType] = useState('bug');
+  const [types, setTypes] = useState([]);
 
   useEffect(() => {
     const loadData = async () => {
+      console.log('running useEffect', Math.random());
 
       try {
-        const data = await fetchPokemon();
+        const data = await fetchPokemon(selectedType);
         setPokemon(data);
         setLoading(false);
       } catch (e) {
@@ -17,7 +20,18 @@ export function usePokemon() {
       }
     };
     loadData();
-  }, []);
+  }, [selectedType]);
 
-  return { loading, pokemon };
+  useEffect(() => {
+    const loadTypes = async () => {
+      try {
+        const data = await fetchTypes();
+        setTypes(data.map((type) => type.type));
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    loadTypes();
+  }, []);
+  return { loading, pokemon, types, setSelectedType };
 }
--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,13 +1,18 @@
-import React from 'react';
-
-export default function pokemon() {
-  return (
-    <div>pokemon</div>
+export async function fetchPokemon(selectedType) {
+  const params = new URLSearchParams();
+  if (selectedType !== 'all') {
+    params.set('type', selectedType);
+  }
+  console.log(params.toString());
+  const resp = await fetch(
+    `https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}`
   );
+  const data = await resp.json();
+  return data.results;
 }
 
-export async function fetchPokemon() {
-  const resp = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex`);
+export async function fetchTypes() {
   const data = await resp.json();
+  const resp = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex`);
   return data.results;
 }
